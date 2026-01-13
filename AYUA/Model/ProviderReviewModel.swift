30c6f141e2bcdc6d865c4c9f13c9fd1ac937d7d4
Rohit Singh Dhakad  [C] <rohitsingh.dhakad@mastercard.com>
2026-01-13 23:27:41 +0530

    Added remaning api

---
+++ AYUA/Model/ProviderReviewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProviderReviewModel.swift
+//  AYUA
+//
+//  Created by Rohit Singh Dhakad  [C] on 13/01/26.
+//
+
+import Foundation
---
+++ AYUA/ViewController/MyReview/UsersReviewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UsersReviewViewController.swift
+//  AYUA
+//
+//  Created by Rohit Singh Dhakad  [C] on 13/01/26.
+//
+
+import UIKit
+
+class UsersReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
